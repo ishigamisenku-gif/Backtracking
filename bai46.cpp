326733eb822d13b39b715c8382cccef7ab1a3ff1
ishigamisenku-gif <anhkhuehuynh77@gmail.com>
2026-08-16 14:18:44 +0700

    tenshii-samaa

---
+++ bai46.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+ll n;
+void gen(ll i, string s){
+    if (i==n){
+        cout<<s<<"\n";
+        return;
+    }
+    s[i]='0';
+    gen(i+1,s);
+    s[i]='1';
+    gen(i+1,s);
+
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cin>>n;
+    string s(n,'0');
+    gen(0,s);
+    return 0;
+}
