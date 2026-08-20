2b20ddddf96da4f455cc93e6c0cbc5b1959c5555
ishigamisenku-gif <anhkhuehuynh77@gmail.com>
2026-08-20 09:40:29 +0700

    itterashai

---
+++ bai54.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define problem ""
+
+ll n;
+vector<ll> cur;
+vector<bool> vis;
+
+bool check(const vector<ll>&a){
+    for (int i=0;i<n;i++){
+        if (a[i]==i+1)return false;
+    }
+    return true;
+}
+
+void gen(ll k){
+    if(cur.size()==k){
+        if (check(cur)){for(int i=0;i<cur.size();i++) cout<<cur[i]<<" ";
+        cout<<"\n";}
+        return;
+    }
+    for(int i=1;i<=n;i++){
+        if(vis[i]) continue;
+        vis[i]=true;
+        cur.push_back(i);
+        gen(k);
+        cur.pop_back();
+        vis[i]=false;
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    if(fopen(problem".INP","r")){
+        freopen(problem".INP","r",stdin);
+        freopen(problem".OUT","w",stdout);
+    }
+    cin>>n;
+    vis.assign(n+1,false);
+    gen(n);
+    return 0;
+}
